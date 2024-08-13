28584fd119cb0b7c9a062db87095b7ee2559da44
NinaMadu <136308860+NinaMadu@users.noreply.github.com>
2024-08-13 23:01:53 +0530

    profile update 2

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -21,7 +21,8 @@ const Profile = () => {
   const dispatch = useDispatch();
   const [showProfile, setShowProfile] = useState(true);
   const [showUserItems, setShowUserItems] = useState(false);
-
+  const [showWishlist, setShowWishlist] = useState(false);
+  
   useEffect(() => {
     if (file) {
       handleFileUpload(file);
@@ -129,6 +130,7 @@ const Profile = () => {
       setUserListings(data);
       setShowProfile(false);
       setShowUserItems(true);
+      setShowWishlist(false);
     } catch (error) {
       setShowListingsError(true);
     }
@@ -160,6 +162,9 @@ const Profile = () => {
         return;
       }
       setWishlist(data.wishlist);
+      setShowProfile(false);
+        setShowUserItems(false); 
+        setShowWishlist(true); 
     } catch (error) {
       setShowWishlistError(true);
     }
@@ -172,69 +177,120 @@ const Profile = () => {
 
   return (
     <div className='w-full'>
-      <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
+      <h1 className='text-3xl font-semibold text-left mx-5 my-7'>Hello {currentUser.username ? currentUser.username : 'User'}!</h1>
       <div className='w-full grid grid-cols-1 md:grid-cols-4 lg:grid-cols-4 gap-8'>
+        
+        <div className='col-span-1 md:col-span-1 lg:col-span-1 p-3'>
+          <div className='flex flex-col gap-4'>
+            <button
+              onClick={() => setShowProfile(true)}
+              className='bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95'>
+              Update Profile
+            </button>
+            <Link
+              className='bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95'
+              to={"/create-listing"}>
+              Create a free Ad
+            </Link>
+            <button
+              onClick={handleShowListing}
+              className='bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95'>
+              My Ads
+            </button>
+            <button
+              onClick={fetchWishlistData}
+              className='bg-blue-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95'>
+              My Wishlist
+            </button>
+            <button
+              onClick={handleSignOut}
+              className='bg-red-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95'>
+              Sign out
+            </button>
+            <button
+              onClick={handleDeleteUser}
+              className='bg-red-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95'>
+              Delete account
+            </button>
+            <p className='text-red-700 mt-5'>{showListingsError ? 'Error in Listings' : ''}</p>
+            <p className='text-red-700 mt-5'>{showWishlistError ? 'Error in Wishlist' : ''}</p>
+          </div>
+        </div>
+
         <div className='col-span-1 md:col-span-3 lg:col-span-3 p-3'>
-          {showProfile ? (
-            <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
-              <input
-                onChange={(e) => setFile(e.target.files[0])}
-                type='file'
-                ref={fileRef}
-                hidden
-                accept='image/*'
-              />
-              <img
-                onClick={() => fileRef.current.click()}
-                src={formData.avatar || currentUser.avatar}
-                alt='avatar'
-                className='w-64 h-64 rounded-full object-cover cursor-pointer self-center mt-2'
-              />
-              <p className='text-sm self-center'>
-                {fileUploadError ? (
-                  <span className='text-red-700'>Error Image Upload (image must be less than 2 MB)</span>
-                ) : filePerc > 0 && filePerc < 100 ? (
-                  <span className='text-slate-700'>{`Uploading ${filePerc}%`}</span>
-                ) : filePerc === 100 ? (
-                  <span className='text-green-700'>Image uploaded successfully</span>
-                ) : (
-                  ''
-                )}
-              </p>
-              <input
-                type='text'
-                placeholder='username'
-                id='username'
-                defaultValue={currentUser.username}
-                onChange={handleChange}
-                className='border p-3 rounded-lg'
-              />
-              <input
-                type='text'
-                placeholder='email'
-                id='email'
-                defaultValue={currentUser.email}
-                onChange={handleChange}
-                className='border p-3 rounded-lg'
-              />
-              <input
-                type='text'
-                placeholder='password'
-                onChange={handleChange}
-                id='password'
-                className='border p-3 rounded-lg'
-              />
-              <button className='bg-slate-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95'>
-                {loading ? 'Loading...' : 'Update'}
-              </button>
-              <p className='text-red-700 mt-5'>{error ? error : ''}</p>
-              <p className='text-green-700 mt-5'>{updateSuccess ? 'Profile updated successfully!' : ''}</p>
-            </form>
-          ) : showUserItems ? (
+        {showProfile ? (
+          <div className='flex flex-col gap-4'>
+                  <h1 className='text-center mt-0 text-2xl font-semibold'>Your Profile</h1>
+          <form onSubmit={handleSubmit} className='flex flex-col md:grid md:grid-cols-2 gap-4'>
+            
+            <div className='flex flex-col items-center'>
+            <input
+              onChange={(e) => setFile(e.target.files[0])}
+              type='file'
+              ref={fileRef}
+              hidden
+              accept='image/*'
+            />
+            <img
+              onClick={() => fileRef.current.click()}
+              src={formData.avatar || currentUser.avatar}
+              alt='avatar'
+              className='w-64 h-64 rounded-full object-cover cursor-pointer self-center mt-2'
+            />
+            <p className='text-sm self-center'>
+              {fileUploadError ? (
+                <span className='text-red-700'>
+                  Error Image Upload (image must be less than 2 MB)
+                </span>
+              ) : filePerc > 0 && filePerc < 100 ? (
+                <span className='text-slate-700'>{`Uploading ${filePerc}%`}</span>
+              ) : filePerc === 100 ? (
+                <span className='text-green-700'>Image uploaded successfully</span>
+              ) : (
+                ''
+              )}
+            </p>
+            </div>
+            <div className='flex flex-col gap-4'>
+            <input
+              type='text'
+              placeholder='username'
+              id='username'
+              defaultValue={currentUser.username}
+              onChange={handleChange}
+              className='border p-3 rounded-lg'
+            />
+            <input
+              type='text'
+              placeholder='email'
+              id='email'
+              defaultValue={currentUser.email}
+              onChange={handleChange}
+              className='border p-3 rounded-lg'
+            />
+            <input
+              type='password'
+              placeholder='password'
+              onChange={handleChange}
+              id='password'
+              className='border p-3 rounded-lg'
+            />
+            <button className='bg-slate-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95'>
+              {loading ? 'Loading...' : 'Update'}
+            </button>
+            <p className='text-red-700 mt-5'>{error ? error : ''}</p>
+            <p className='text-green-700 mt-5'>
+              {updateSuccess ? 'Profile updated successfully!' : ''}
+            </p>
+            </div>
+          </form>
+          </div>
+        ) : showUserItems ? (
             <div>
               {userListings && userListings.length > 0 && (
+                <div>
                 <div className='flex flex-col gap-4'>
-                  <h1 className='text-center mt-7 text-2xl font-semibold'>Your Listings</h1>
+                  <h1 className='text-center mt-0 text-2xl font-semibold'>Your Listings</h1>
                   {userListings.map((listing) => (
                     <div
                       key={listing._id}
@@ -266,10 +322,18 @@ const Profile = () => {
                     </div>
                   ))}
                 </div>
+                </div>
+                
               )}
-              {wishlist && wishlist.length > 0 && (
+              </div>
+              
+              
+        ) : showWishlist ? ( 
+          <div>
+            
+            {wishlist && wishlist.length > 0 && (
                 <div className='flex flex-col gap-4'>
-                  <h1 className='text-center mt-7 text-2xl font-semibold'>Your Wishlist</h1>
+                  <h1 className='text-center mt-0 text-2xl font-semibold'>Your Wishlist</h1>
                   {wishlist.map((item) => (
                     <div
                       key={item._id}
@@ -300,42 +364,7 @@ const Profile = () => {
             </div>
           ) : null}
         </div>
-        <div className='col-span-1 md:col-span-1 lg:col-span-1 p-3'>
-          <div className='flex flex-col gap-4'>
-            <button
-              onClick={() => setShowProfile(true)}
-              className='bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95'>
-              Update Profile
-            </button>
-            <Link
-              className='bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95'
-              to={"/create-listing"}>
-              Create a free Ad
-            </Link>
-            <button
-              onClick={handleShowListing}
-              className='bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95'>
-              My Ads
-            </button>
-            <button
-              onClick={fetchWishlistData}
-              className='bg-blue-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95'>
-              My Wishlist
-            </button>
-            <button
-              onClick={handleDeleteUser}
-              className='bg-red-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95'>
-              Delete account
-            </button>
-            <button
-              onClick={handleSignOut}
-              className='bg-red-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95'>
-              Sign out
-            </button>
-            <p className='text-red-700 mt-5'>{showListingsError ? 'Error in Listings' : ''}</p>
-            <p className='text-red-700 mt-5'>{showWishlistError ? 'Error in Wishlist' : ''}</p>
-          </div>
-        </div>
+
       </div>
     </div>
   );
